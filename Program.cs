194ffbefa3571b30157b350c17ee81d5fb629216
Artem468 <56316884+Artem468@users.noreply.github.com>
2023-09-27 23:13:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(y >= -x + 1 && y <= x + 1 && y >= x - 1 && y <= -x + 3);
